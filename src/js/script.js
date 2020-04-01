20a83ecc0cd74373c3e7b4c33abfa13cd2f4e42f
Max <max_lisyanskiy@mail.ru>
2020-04-01 14:54:55 +0300

    mailer

--- src/js/script.js
+++ src/js/script.js
@@ -84,6 +84,21 @@ $(document).ready(function(){
     // Mask
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
     // Smooth scroll and page up
     $(window).scroll(function() {
         if ($(this).scrollTop() > 1000) {
